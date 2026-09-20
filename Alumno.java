d29562b87b9599dfb029a45ba4223048fcb0a38d
alanseoane <alansg2208@gmail.com>
2026-09-20 19:03:26 -0300

    agrega descripcion() a Alumno

--- Alumno.java
+++ Alumno.java
@@ -25,5 +25,10 @@ public class Alumno {
     public Alumno(String nombreAlumno, String apellidoAlumno, String dniAlumno) {
         this(nombreAlumno, apellidoAlumno, dniAlumno, "principiante");
     }
+}
+
+    public String descripcion() {
+        return "Alumno: " + nombreAlumno + " "  + apellidoAlumno + " - DNI: " + dniAlumno + " - Nivel: " + nivelAlumno + " - Número de alumno: " + numeroAlumno;
+    }
 
 }
