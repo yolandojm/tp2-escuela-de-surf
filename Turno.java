f883063d1e3560d1318e824c527bdc1ed42bf61f
alanseoane <alansg2208@gmail.com>
2026-09-20 19:49:40 -0300

    implementa arreglo de inscripciones en Turno

--- Turno.java
+++ Turno.java
@@ -2,6 +2,8 @@ public class Turno {
     private String fechaTurno;
     private String horarioTurno;
     private Instructor instructor;
+    private Inscripcion[] inscripciones;
+    private int cantidadInscripciones;
 
     public static final int CUPO_MAXIMO = 10;
 
@@ -9,6 +11,12 @@ public class Turno {
         this.fechaTurno = fechaTurno;
         this.horarioTurno = horarioTurno;
         this.instructor = instructor;
+        this.inscripciones = new Inscripcion[CUPO_MAXIMO];
+    }
+
+    public void agregarInscripcion(Inscripcion inscripcion) {
+        inscripciones[cantidadInscripciones] = inscripcion;
+        cantidadInscripciones++;
     }
 
     public String descripcion() {
