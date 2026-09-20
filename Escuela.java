30870d00409efdd21789343a5623060dc8e8523a
alanseoane <alansg2208@gmail.com>
2026-09-20 19:43:14 -0300

    implementa arreglo de turnos en Escuela

--- Escuela.java
+++ Escuela.java
@@ -1,12 +1,20 @@
 public class Escuela {
     private String nombreEscuela;
     private String playa;
+    private Turno[] turnos;
+    private int cantidadTurnos;
 
     public Escuela(String nombreEscuela, String playa) {
         this.nombreEscuela = nombreEscuela;
         this.playa = playa;
+        this.turnos = new Turno[50];
     }
     public String descripcion(){
         return "Escuela: " + nombreEscuela + " - playa: " + playa;
+    }
+
+    public void agregarTurno(Turno turno) {
+        turnos[cantidadTurnos] = turno;
+        cantidadTurnos++;
     }
 }
