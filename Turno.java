546c58a07c3e14e0f4a2b1f0a8f711d3550b612f
yolandojm <mariano-yolando@hotmail.com>
2026-09-20 18:24:44 -0300

    cupo maximo por turno

--- Turno.java
+++ Turno.java
@@ -1,22 +1,14 @@
 public class Turno {
     private String fechaTurno;
     private String horarioTurno;
-    private int cupoMaximo;
     private Instructor instructor;
 
-    public Turno(String fechaTurno, String horarioTurno, int cupoMaximo, Instructor instructor) {
+    public static final int CUPO_MAXIMO = 10;
+
+    public Turno(String fechaTurno, String horarioTurno, Instructor instructor) {
         this.fechaTurno = fechaTurno;
         this.horarioTurno = horarioTurno;
-        setCupoMaximo(cupoMaximo);
         this.instructor = instructor;
-    }
-
-    public void setCupoMaximo(int cupoMaximo) {
-        if (cupoMaximo >= 1) {
-            this.cupoMaximo = cupoMaximo;
-        } else {
-            throw new IllegalArgumentException("El cupo de un turno no puede ser menor que 1.");
-        }
     }
 
 }
