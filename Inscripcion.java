6c0d7078b69003888ad85ee51999da35ffe2b74f
yolandojm <mariano-yolando@hotmail.com>
2026-09-21 18:04:04 -0300

    Agrega atributo Turno a Inscripcion

--- Inscripcion.java
+++ Inscripcion.java
@@ -3,12 +3,14 @@ public class Inscripcion {
     private boolean asistencia;
     private Tabla tablaAsignada;
     private Alumno alumno;
+    private Turno turno;
 
-    public Inscripcion(String fechaInscripcion, boolean asistencia, Tabla tablaAsignada, Alumno alumno) {
+    public Inscripcion(String fechaInscripcion, boolean asistencia, Tabla tablaAsignada, Alumno alumno, Turno turno) {
         this.fechaInscripcion = fechaInscripcion;
         this.asistencia = asistencia;
         this.tablaAsignada = tablaAsignada;
         this.alumno = alumno;
+        this.turno = turno;
     }
 
 }
